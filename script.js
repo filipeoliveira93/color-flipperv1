e67d37c892cc63b2ea4d216da58cceec8d3a7713
Filipe Oliveira <filipeoliveira0426@gmail.com>
2022-02-17 18:14:11 -0300

    1.2

--- script.js
+++ script.js
@@ -3,24 +3,16 @@ const colorRGB = document.getElementById("color-rgb");
 const colorHEX = document.getElementById("color-hex");
 const copyBtn = document.querySelectorAll(".copy");
 const alert = document.getElementById("alert");
-console.log(alert.classList)
-style = document.getElementById('alert').classList
-console.log(style);
+
 copyBtn.forEach((btn) => {
 	btn.addEventListener("click", (e) => {
-		const test = e.currentTarget.parentElement.querySelector("input");
-		console.log(test.value);
-
-        // test.setSelectionRange(0, 99999); //para mobile
-        navigator.clipboard.writeText(test.value);
+		const copycolor = e.currentTarget.parentElement.querySelector("input");
+		console.log(copycolor.value);
+        navigator.clipboard.writeText(copycolor.value);
         alert.classList.add("alert-show");
         setTimeout(() => {
-            console.log('teste')
             alert.classList.remove("alert-show");
-        }, 500);
-
-        console.log(alert);
-        
+        }, 500);        
 	});
 });
 
@@ -35,12 +27,8 @@ btn.addEventListener("click", () => {
 	const g = randomRGB();
 	const b = randomRGB();
 	let rgb = "rgb(" + r + ", " + g + ", " + b + ")";
-
-	console.log(rgb);
 	let hex = "#" + r.toString(16) + g.toString(16) + b.toString(16);
     hex = hex.toUpperCase()
-    
-    console.log(hex);
 	document.body.style.backgroundColor = rgb;
 	colorRGB.value = rgb;
 	colorHEX.value = hex;
